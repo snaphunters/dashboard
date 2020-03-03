9c91810a2f622feb10d8fed466163e388a5865a4
nevtay <nevintay@users.noreply.github.com>
2020-03-03 14:31:42 +0800

    [nev/val] #29 Remove code comments triggering eslint warnings

--- src/tests/Dashboard.test.js
+++ src/tests/Dashboard.test.js
@@ -1,18 +1,4 @@
 import React from "react";
 import { render } from "@testing-library/react";
-//import App from "./container/Dashboard";
 
 test.todo("Do something");
-
-// describe("Dashboard.js", () => {
-//   test("Dashboard should render", () => {
-//     const { getByText } = render(<App />);
-//     const dashBoardComponent = getByText(/Dashboard/);
-//     expect(dashBoardComponent).toBeInTheDocument();
-//   });
-//   test("Button link to Editor.js should render", () => {
-//     const { getByLabelText } = render(<App />);
-//     const addNewArticleButton = getByLabelText(/Create New Article/);
-//     expect(addNewArticleButton).toBeInTheDocument();
-//   });
-// });
