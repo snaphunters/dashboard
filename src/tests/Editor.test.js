b3ab6362042e2037a206f6fa27839d98cd8959e9
Chong Ren <leechongren@users.noreply.github.com>
2020-03-05 17:27:12 +0800

    [CR/Nev] #32 add new tests; rename arguments and consts to reflect changes to code

--- src/tests/Editor.test.js
+++ src/tests/Editor.test.js
@@ -26,19 +26,48 @@ describe("Editor.js", () => {
     expect(articleTitleText).toBeInTheDocument();
   });
   test("TextBox is rendered when add text box button is clicked", () => {
-    const { getByLabelText } = render(<Editor />);
+    const { getByLabelText, getAllByLabelText } = render(<Editor />);
     const insertTextButton = getByLabelText("Add Text Button");
     fireEvent.click(insertTextButton);
-    const textBlock = getByLabelText("Text Block");
-    expect(textBlock).toBeInTheDocument();
+    const textBlock = getAllByLabelText("Article Content Block");
+    expect(textBlock).toHaveLength(2);
   });
   test("TextBox input is rendered on the editor", () => {
-    const { getByLabelText, getByDisplayValue } = render(<Editor />);
+    const { getByLabelText, getAllByLabelText, getByDisplayValue } = render(
+      <Editor />
+    );
     const insertTextButton = getByLabelText("Add Text Button");
     fireEvent.click(insertTextButton);
-    const textBlock = getByLabelText("Text Block");
-    fireEvent.change(textBlock, { target: { value: "Snapi" } });
-    const TextBoxText = getByDisplayValue("Snapi");
-    expect(TextBoxText).toBeInTheDocument();
+    const textBlock = getAllByLabelText("Article Content Block");
+    fireEvent.change(textBlock[1], { target: { value: "SnapiLOL" } });
+    const TextBoxText = getByDisplayValue("SnapiLOL");
+    expect(TextBoxText.value).toEqual("SnapiLOL");
+  });
+
+  test("Article Title <Input> Box is rendered with no text when creating new article", () => {
+    const { getByLabelText } = render(<Editor />);
+    const articleTitleInputBox = getByLabelText("Article Title Input Box");
+    expect(articleTitleInputBox).toBeInTheDocument();
+    expect(articleTitleInputBox.value).toEqual("");
+  });
+  test("Article Content Block is rendered with no text when creating new article", () => {
+    const { getByLabelText } = render(<Editor />);
+    const articleContentBlock = getByLabelText("Article Content Block");
+    expect(articleContentBlock).toBeInTheDocument();
+    expect(articleContentBlock.value).toEqual("");
+  });
+  test("Article Content Block is rendered with no text and uuid v4 string in placeholder creating new article", () => {
+    const { getByLabelText } = render(<Editor />);
+    const articleContentBlock = getByLabelText("Article Content Block");
+    expect(articleContentBlock).toBeInTheDocument();
+    expect(articleContentBlock.placeholder).toMatch(/-/);
+    expect(articleContentBlock.placeholder).toHaveLength(36);
+  });
+  test("Article Content Block renders user-typed values", () => {
+    const { getByLabelText, getByDisplayValue } = render(<Editor />);
+    const articleContentBlock = getByLabelText("Article Content Block");
+    fireEvent.change(articleContentBlock, { target: { value: "lol" } });
+    const articleContentBlockText = getByDisplayValue("lol");
+    expect(articleContentBlockText.value).toEqual("lol");
   });
 });
