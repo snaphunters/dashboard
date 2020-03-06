2ddf9b994a05b6e2d67f0d555773e56e6cb7e509
Nicholas Chung <iamnicholaschung@gmail.com>
2020-03-06 13:42:56 +0800

    [Nev/Nic] #2 Remove unused semantic-react-ui components

--- src/component/SaveErrorModal.js
+++ src/component/SaveErrorModal.js
@@ -3,13 +3,12 @@ import { Header, Modal, Button, Icon } from "semantic-ui-react";
 
 const SavedErrorModal = ({ isSaveError, onHandleSaveError }) => {
   return (
-    // <div> saved modellll {isSaved.toString()} </div>
     <Modal open={isSaveError}>
       <Header content="Save Error" />
       <Modal.Content>
         <p>
-          Error!Your article might have the same title as an existing article no
-          title available.
+          Error! Your article might have the same title as an existing article
+          or there is no title available.
         </p>
       </Modal.Content>
       <Modal.Actions>
