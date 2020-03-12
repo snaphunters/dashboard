2d935e4d63a04dd52325537d7bf3c11129c3383c
weemeng <weemeng@users.noreply.github.com>
2020-03-12 21:45:56 +0800

    [wm] Add React_App_ to react environment variables

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: process.env.SNAPI_HEROKU_BACKEND_URL
+  baseURL: process.env.REACT_APP_SNAPI_HEROKU_BACKEND_URL
 });
 instance.defaults.headers.post["Content-Type"] = "application/json";
 
