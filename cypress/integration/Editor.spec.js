60ccfbcfe57f9f788c8643cab369138e72461729
nevtay <nevintay@users.noreply.github.com>
2020-03-13 13:12:25 +0800

    [Nev] #31 Add test for 'add subtopic container' feature

--- cypress/integration/Editor.spec.js
+++ cypress/integration/Editor.spec.js
@@ -17,4 +17,40 @@ describe("Editor", () => {
     cy.get('[aria-label="Save Button"]').click();
     cy.get('[aria-label="close save message"]').should("be.visible");
   });
+  it("should render another subtopic container when clicking a new 'add subtopic container' button", () => {
+    cy.get('[aria-label="topicSubtopicContainer 1"]').should("be.visible");
+    cy.get('[aria-label="Add subtopic container button 1"]').click();
+    cy.get('[aria-label="topicSubtopicContainer 2"]').should("be.visible");
+    cy.get('[aria-label="Add subtopic container button 2"]').click();
+    cy.get('[aria-label="topicSubtopicContainer 3"]').should("be.visible");
+    cy.get('[aria-label="Add subtopic container button 3"]').click();
+    cy.get('[aria-label="topicSubtopicContainer 4"]').should("be.visible");
+    cy.get('[aria-label="Add subtopic container button 4"]').click();
+    cy.get('[aria-label="topicSubtopicContainer 5"]').should("be.visible");
+  });
+  it("should render new subtopic containers below the correct container", () => {
+    cy.get('[aria-label="topicSubtopicContainer 1"]').should("be.visible");
+    cy.get('[aria-label="Add subtopic container button 1"]').click();
+    cy.get('[aria-label="topicSubtopicContainer 2"]').should("be.visible");
+    cy.get('[aria-label="topicSubtopicContainer 2"]').type(
+      `This block should move to topicSubtopicContainer 3`
+    );
+    cy.get('[aria-label="Add subtopic container button 2"]').click();
+    cy.get('[aria-label="topicSubtopicContainer 3"]').should("be.visible");
+    cy.get('[aria-label="Add subtopic container button 3"]').click();
+    cy.get('[aria-label="topicSubtopicContainer 4"]').should("be.visible");
+    cy.get('[aria-label="Add subtopic container button 4"]').click();
+    cy.get('[aria-label="topicSubtopicContainer 5"]').should("be.visible");
+    cy.get('[aria-label="topicSubtopicContainer 4"]').type(
+      `This block should move to topicSubtopicContainer 6`
+    );
+    cy.get('[aria-label="Add subtopic container button 3"]').click();
+    cy.get('[aria-label="Add subtopic container button 1"]').click();
+    cy.get('[aria-label="topicSubtopicContainer 6"]')
+      .contains(`This block should move to topicSubtopicContainer 3`)
+      .should("be.visible");
+    cy.get('[aria-label="topicSubtopicContainer 3"]')
+      .contains(`This block should move`)
+      .should("be.visible");
+  });
 });
