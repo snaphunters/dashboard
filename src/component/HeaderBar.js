3c4fa4d1fd181aea5e5fae9a9d6b99957e3311df
teojincheng <jinchengproject@gmail.com>
2020-03-18 18:35:27 +0800

    [Nev/JC] #13 Add delete button to navbar in Editor

--- src/component/HeaderBar.js
+++ src/component/HeaderBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Menu } from "semantic-ui-react";
+import { Button, Menu, Container } from "semantic-ui-react";
 const HeaderBar = ({
   isEditable,
   toggleEditable,
@@ -7,7 +7,9 @@ const HeaderBar = ({
   publishTopic,
   addSubtopicContainer,
   topicSubtopicIndex,
-  returnToDash
+  returnToDash,
+  getTitle,
+  openDeleteModal
 }) => (
   <>
     <Menu
@@ -18,43 +20,61 @@ const HeaderBar = ({
       borderless
       size="large"
     >
-      <Menu.Item position="left" aria-label="return to dashboard">
-        <Button onClick={returnToDash} aria-label="Return to Dashboard">
-          Back to Dashboard
-        </Button>
-      </Menu.Item>
-      <Menu.Item>
-        <Button
-          content="Edit"
-          toggle
-          active={isEditable}
-          onClick={() => toggleEditable(true)}
-          aria-label="Edit Button"
-        />
-      </Menu.Item>
-      <Menu.Item>
-        <Button
-          content="Preview"
-          toggle
-          active={!isEditable}
-          onClick={() => toggleEditable(false)}
-          aria-label="Preview Button"
-        />
-      </Menu.Item>
-      <Menu.Item position="right">
-        <Button onClick={saveDraft} aria-label="Save Button">
-          Save as Draft
-        </Button>
-      </Menu.Item>
-      <Menu.Item>
-        <Button
-          onClick={publishTopic}
-          aria-label="Publish Button"
-          color="yellow"
-        >
-          Publish
-        </Button>
-      </Menu.Item>
+      <Container>
+        <Menu.Item position="left" aria-label="return to dashboard">
+          <Button onClick={returnToDash} aria-label="Return to Dashboard">
+            Back to Dashboard
+          </Button>
+        </Menu.Item>
+      </Container>
+
+      <Container>
+        <Menu.Item>
+          <Button
+            content="Edit"
+            toggle
+            active={isEditable}
+            onClick={() => toggleEditable(true)}
+            aria-label="Edit Button"
+          />
+        </Menu.Item>
+        <Menu.Item>
+          <Button
+            content="Preview"
+            toggle
+            active={!isEditable}
+            onClick={() => toggleEditable(false)}
+            aria-label="Preview Button"
+          />
+        </Menu.Item>
+      </Container>
+
+      <Container>
+        <Menu.Item position="right">
+          <Button
+            toggle
+            active={isEditable}
+            onClick={openDeleteModal}
+            aria-label="Remove Article"
+          >
+            Delete
+          </Button>
+        </Menu.Item>
+        <Menu.Item position="right">
+          <Button onClick={saveDraft} aria-label="Save Button">
+            Save as Draft
+          </Button>
+        </Menu.Item>
+        <Menu.Item position="right">
+          <Button
+            onClick={publishTopic}
+            aria-label="Publish Button"
+            color="yellow"
+          >
+            Publish
+          </Button>
+        </Menu.Item>
+      </Container>
     </Menu>
   </>
 );
